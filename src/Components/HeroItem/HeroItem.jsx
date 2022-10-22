bd14cc17990453d3fc2ba2e0e4d33c2426218bb8
mccstrategymarketing.com.ua <mccstrategymarketing.com.ua>
2022-10-22 15:31:59 +0300

    fix image url

--- src/Components/HeroItem/HeroItem.jsx
+++ src/Components/HeroItem/HeroItem.jsx
@@ -6,7 +6,7 @@ export default function HeroItem({ nickname, img_hero, _id }) {
     <div className={style.hero_item}>
       <Link to={`/hero/${_id}`}>
         <h2>{nickname}</h2>
-        <img src={`${process.env.REACT_APP_API_URL}${img_hero}`} alt='' />
+        <img src={`${process.env.REACT_APP_API_URL}/${img_hero}`} alt='' />
       </Link>
     </div>
   );
